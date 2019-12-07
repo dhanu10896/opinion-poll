4f46cf353de3ad704eb80194f2ed3872a28b7c21
Dhananjay Nagargoje <dhananjay.nagargoje@sap.com>
2019-12-07 11:43:24 +0530

    opinion poll spring boot app

---
+++ src/main/java/com/fullstackwebapp/opinionpoll/PoolingApplication.java
@@ -0,0 +1,28 @@
+package com.fullstackwebapp.opinionpoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+@EntityScan(basePackageClasses = {
+        PoolingApplication.class,
+        Jsr310JpaConverters.class
+})
+
+public class PoolingApplication {
+
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoolingApplication.class, args);
+    }
+
+}
