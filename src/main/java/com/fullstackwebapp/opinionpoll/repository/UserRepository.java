acee0d55b80b4d6e8eea4fe0c38ea2b9f131d014
Dhananjay Nagargoje <dhananjay.nagargoje@sap.com>
2019-12-08 15:44:28 +0530

    repositories added

---
+++ src/main/java/com/fullstackwebapp/opinionpoll/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.fullstackwebapp.opinionpoll.repository;
+
+import com.fullstackwebapp.opinionpoll.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface  UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+
+    User findByUsernameOrEmail(String username, String email);
+
+    List<User> findByIdIn(List<Long> userIds);
+
+    User findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
